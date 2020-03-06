1ed115a68061cfdebc6a2fe6dd1cdc29ab76a7e6
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-06 13:52:22 +0800

    Use function to parse argument

--- autoeq-explorer.py
+++ autoeq-explorer.py
@@ -1,25 +1,38 @@
 import argparse
 
-argParser = argparse.ArgumentParser()
+# Place some placeholders
+args = None
+# Read command line arguments
 
-# MultiHeadphone
-argParser.add_argument("-m", "--multiHeadphone",
-                       help="Force specify multi headphone mode",
-                       action="store_true")
-# Overwrite
-argParser.add_argument("-o", "--overwrite",
-                       help="Overwrite existing results",
-                       action="store_true")
-# AllToAll
-argParser.add_argument("-a", "--allToAll",
-                       help="Turn on all to all preprocessor",
-                       action="store_true")
 
-args = argParser.parse_args()
+def pythonArgParser():
+    global args
 
-# Scan flags
-# MUST apply all flags here before enter code
-# the arguments should not change by code after scan flags
-if args.allToAll is True:
-    args.multiHeadphone = True
-    args.override = False
+    argParser = argparse.ArgumentParser()
+
+    # MultiHeadphone
+    argParser.add_argument("-m", "--multiHeadphone",
+                           help="Force specify multi headphone mode",
+                           action="store_true")
+    # Overwrite
+    argParser.add_argument("-o", "--overwrite",
+                           help="Overwrite existing results",
+                           action="store_true")
+    # AllToAll
+    argParser.add_argument("-a", "--allToAll",
+                           help="Turn on all to all preprocessor",
+                           action="store_true")
+
+    args = argParser.parse_args()
+
+# Preprocess argument
+# the argument should not change by code after preprocess
+
+
+def myArgParser():
+    pythonArgParser()
+
+    if args.allToAll is True:
+        args.multiHeadphone = True
+        args.override = False
+
