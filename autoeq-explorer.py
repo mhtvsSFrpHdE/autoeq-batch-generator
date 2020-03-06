a43da4ee6138119d12f6f48e878193c1b20c2f6f
mhtvsSFrpHdE <2bemperor47@gmail.com>
2020-03-06 11:50:16 +0800

    Add comment

--- autoeq-explorer.py
+++ autoeq-explorer.py
@@ -1,11 +1,12 @@
 import argparse
-#multiHeadphone
-#overwrite
 
 argParser = argparse.ArgumentParser()
+
+# MultiHeadphone
 argParser.add_argument("-m", "--multiHeadphone",
                        help="Force specify multi headphone mode",
                        action="store_true")
+# Overwrite
 argParser.add_argument("-o", "--overwrite",
                        help="Overwrite existing results",
                        action="store_true")
